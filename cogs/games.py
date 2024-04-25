0ece28f600b6ef015c324ad6343ef4c5fa26cb84
Anonymous <lick@my.balls>
2024-04-25 01:06:48 +0000

    stupidness

--- cogs/games.py
+++ cogs/games.py
@@ -22,15 +22,15 @@ class Games(commands.Cog):
         #remove links
         string = string.replace("https://", "").replace("http://", "")
         #reformat mentions
-        pattern = r'<@\d+>'
+        pattern = r'<@*\d+>'
         output_string = re.sub(pattern, lambda match: guild.get_member(int(match.group(0).split('@')[1][:-1])).display_name, string)
         return output_string
 
     @app_commands.command(name="game-event",description="Ping people with a specific game role.")
-    async def game_event(self, interaction: discord.Interaction, game_title:str='',custom_msg:str=''):
+    async def game_event(self, interaction: discord.Interaction, game_title:str,custom_msg:str):
         user = interaction.user
 
-        if game_title == '' or game_title not in self.bot.gamemap:
+        if game_title not in self.bot.gamemap:
             await interaction.response.send_message(f"To ping a game, re-send this command with one of the available ping roles in `game_title`. Case sensitive.\n\n- " + "\n- ".join(self.bot.gamemap.keys()),ephemeral=True)
             return
         else:
@@ -39,6 +39,7 @@ class Games(commands.Cog):
             if game_role.id not in roles:
                 await interaction.response.send_message(f"You do not have the role for {game_title}! Ask Robert to give it to you.",ephemeral=True)
                 return
+
         if game_title not in self.ping_cooldowns:
             self.ping_cooldowns[game_title] = 0
 
@@ -51,8 +52,9 @@ class Games(commands.Cog):
             return
         self.ping_cooldowns[game_title] = time.time()
         sanitized_msg = self.sanitize_string(custom_msg,self.bot.guild)
-        message = f"{game_role.mention} - User {user.mention} set up an event - {sanitized_msg}"
-        await interaction.response.send_message(message)
+        message = f"{game_role.mention} - User {user.mention} pinged for an event - {sanitized_msg}"
+        await interaction.channel.send(message)
+        await interaction.response.send_message("Event posted!")
 
 async def setup(bot):
     await bot.add_cog(Games(bot), guild = discord.Object(id = bot.guild_id))
